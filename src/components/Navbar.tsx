bb1d3d76f686501faeada63885f6e0f4beab36b2
Africk-Hunter <hunterafrick@gmail.com>
2024-10-24 11:54:43 -0700

    Pages & Styles Setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <a href='/services'>Hi</a>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/services.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+/* import Layout from '../components/Layout'; */
+import '../styles/index.scss'; // Import your Sass styles
+
+const LandingPage: React.FC = () => {
+  return (
+    <div>hello</div>
+  );
+};
+
+export default LandingPage;
