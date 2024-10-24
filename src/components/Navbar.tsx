a27d716712f78f4e24fd3c898a18d1e184ce33a8
Africk-Hunter <hunterafrick@gmail.com>
2024-10-24 13:04:37 -0700

    Navbar Skeleton HTML
    - Added skeleton HTML to the navbar page
    - Setup SCSS file for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Navbar: React.FC = () => {
+
+  const [windowWidth, setWindowWidth] = useState(0)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+      console.log(window.innerWidth)
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <a href='/services'>Hi</a>
+
+    <section className='navbar' id='navbar'>
+      {windowWidth < 768 ? (
+        <>
+          <section className='topBarWrapper mobile'>
+            <h1 className='companyName'>Luna Lash & Beauty Co.</h1>
+            <button className='hamburgerButton'><img src="" alt="Dropdown Menu" /></button>
+          </section>
+          <nav className='navBarLinks mobile'>
+            <NavLinkButton windowWidth={windowWidth} name='Home' />
+            <NavLinkButton windowWidth={windowWidth} name='Services' />
+            <NavLinkButton windowWidth={windowWidth} name='Gallery' />
+            <NavLinkButton windowWidth={windowWidth} name='About' />
+            <NavLinkButton windowWidth={windowWidth} name='Gallery' />
+          </nav>
+        </>
+      ) : (
+        <>
+          <section className='topBarWrapper desktop'>
+            <h1 className='companyName'>Luna Lash & Beauty Co.</h1>
+            <nav className='navBarLinks desktop'>
+              <NavLinkButton windowWidth={windowWidth} name='Home' />
+              <NavLinkButton windowWidth={windowWidth} name='Services' />
+              <NavLinkButton windowWidth={windowWidth} name='Gallery' />
+              <NavLinkButton windowWidth={windowWidth} name='About' />
+              <NavLinkButton windowWidth={windowWidth} name='Gallery' />
+            </nav>
+          </section>
+        </>
+      )}
+    </section>
   );
 };
+
+interface NavLinkButtonProps {
+  windowWidth: number;
+  name: string;
+}
+
+const NavLinkButton: React.FC<NavLinkButtonProps> = ({ windowWidth, name }) => {
+
+  return (
+    <>
+      {windowWidth < 768 ? (
+        <button className='navLinkButton mobile'>{name}</button>
+      ) : (
+        <button className='navLinkButton desktop'>{name}</button>
+      )}
+    </>
+  );
+
+}
 
 export default Navbar;
