50e1eeb42cd5ec2902afb336c905c1678aae3697
Africk-Hunter <hunterafrick@gmail.com>
2024-10-26 13:17:42 -0700

    Completed Navbar
    - Added styling to navbar for mobile and desktop version (Still need to do large desktop)
    - Refactored Navbar code to be more modular/cleaner

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,49 +7,45 @@ const Navbar: React.FC = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      setWindowWidth(window.innerWidth)
-      console.log(window.innerWidth)
+      setWindowWidth(window.innerWidth);
     };
+    setWindowWidth(window.innerWidth);
     window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener('popstate', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('popstate', handleResize);
+    };
   }, []);
 
-  function toggleHamburger(){
-    if(document.getElementById('navBarLinks')?.classList.contains('open')){
-      document.getElementById('navBarLinks')?.classList.remove('open');
+  function toggleHamburger() {
+
+    const navLinks = document.getElementById('navBarLinks');
+
+    if (navLinks?.classList.contains('open')) {
+      navLinks?.classList.remove('open');
       return;
     }
-    document.getElementById('navBarLinks')?.classList.add('open');
+    navLinks?.classList.add('open');
   }
 
   return (
+
 
     <section className='navbar' id='navbar'>
       {windowWidth < 768 ? (
         <>
           <section className='topBarWrapper mobile'>
             <h1 className='companyName'>Luna Lash & Beauty Co.</h1>
-            <button className='hamburgerButton' onClick={toggleHamburger}><img src={hamburger} alt="Dropdown Menu" className='hamburgerImg'/></button>
+            <button className='hamburgerButton' onClick={toggleHamburger}><img src={hamburger} alt="Dropdown Menu" className='hamburgerImg' /></button>
           </section>
-          <nav className='navBarLinks mobile' id='navBarLinks'>
-            <NavLinkButton windowWidth={windowWidth} name='Home' />
-            <NavLinkButton windowWidth={windowWidth} name='Services' />
-            <NavLinkButton windowWidth={windowWidth} name='Gallery' />
-            <NavLinkButton windowWidth={windowWidth} name='About' />
-            <NavLinkButton windowWidth={windowWidth} name='FAQ' />
-          </nav>
+          <NavBarLinksWrapper windowWidth={windowWidth} names={['Home', 'Services', 'Gallery', 'About', 'FAQ']} />
         </>
       ) : (
         <>
           <section className='topBarWrapper desktop'>
             <h1 className='companyName'>Luna Lash & Beauty Co.</h1>
-            <nav className='navBarLinks desktop'>
-              <NavLinkButton windowWidth={windowWidth} name='Home' />
-              <NavLinkButton windowWidth={windowWidth} name='Services' />
-              <NavLinkButton windowWidth={windowWidth} name='Gallery' />
-              <NavLinkButton windowWidth={windowWidth} name='About' />
-              <NavLinkButton windowWidth={windowWidth} name='FAQ' />
-            </nav>
+            <NavBarLinksWrapper windowWidth={windowWidth} names={['Home', 'Services', 'Gallery', 'About', 'FAQ']} />
           </section>
         </>
       )}
@@ -57,19 +53,47 @@ const Navbar: React.FC = () => {
   );
 };
 
-interface NavLinkButtonProps {
-  windowWidth: number;
-  name: string;
-}
 
-const NavLinkButton: React.FC<NavLinkButtonProps> = ({ windowWidth, name }) => {
+interface NavBarLinksProps {
+  windowWidth: number;
+  names: Array<string>;
+}
+const NavBarLinksWrapper: React.FC<NavBarLinksProps> = ({ windowWidth, names }) => {
+
+  const linkItems = names.map((itemName, index) =>
+    <NavLinkButton windowWidth={windowWidth} name={itemName} key={index} />
+  )
 
   return (
     <>
       {windowWidth < 768 ? (
-        <button className='navLinkButton mobile'>{name}</button>
+        <nav className='navBarLinks mobile' id='navBarLinks'>
+          {linkItems}
+        </nav>
       ) : (
-        <button className='navLinkButton desktop'>{name}</button>
+        <nav className='navBarLinks desktop'>
+          {linkItems}
+        </nav>
+      )}
+    </>
+  );
+}
+
+
+interface NavLinkButtonProps {
+  windowWidth: number;
+  name: string;
+}
+const NavLinkButton: React.FC<NavLinkButtonProps> = ({ windowWidth, name }) => {
+
+  const linkDirect = './' + name.toLowerCase();
+
+  return (
+    <>
+      {windowWidth < 768 ? (
+        <a href={linkDirect} className='navLinkButton mobile'>{name}</a>
+      ) : (
+        <a href={linkDirect} className='navLinkButton desktop'>{name}</a>
       )}
     </>
   );
