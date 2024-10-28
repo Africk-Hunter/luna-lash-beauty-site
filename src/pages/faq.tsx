62dc6e7608076972c68c7815ecc2ec1059185d57
Africk-Hunter <hunterafrick@gmail.com>
2024-10-27 21:40:52 -0700

    Empty pages fix

---
+++ src/pages/faq.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FAQ: React.FC = () => {
+  return (
+    <div>
+      <h1>Frequently Asked Questions</h1>
+      <p>This is the FAQ page.</p>
+    </div>
+  );
+};
+
+export default FAQ;
