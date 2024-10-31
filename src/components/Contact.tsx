e4cd7c5296652d055c8101f42bcbd8412eda1116
Africk-Hunter <hunterafrick@gmail.com>
2024-10-31 12:47:03 -0700

    Contact Secction Styled
    - Added styling for contact section for mobile and desktop devices.
    
    TO DO: Add email functionality for contact form.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+const contactInfo = ['(530) 208-8172', '@Lunalashandbeautyco', '6165 Ridgeview Court, Reno']
+const hours = ['Monday - Closed', 'Tuesday - 11:00am - 7:00pm', 'Wednesday - 11:00am - 7:00pm', 'Thursday - 11:00am - 7:00pm', 'Friday - 11:00am - 7:00pm', 'Saturday - 10:00am - 4:00pm', 'Sunday - Closed']
+
 const Contact: React.FC = () => {
   return (
     <div className="contact" id='contact'>
@@ -8,8 +11,8 @@ const Contact: React.FC = () => {
           <ContactForm />
         </section>
         <section className="companyInformationWrapper">
-          {/* Contact us */}
-          {/* Our Hours */}
+          <HeaderAndText header='Contact Us' texts={contactInfo} />
+          <HeaderAndText header='Our Hours' texts={hours} />
         </section>
       </div>
     </div>
@@ -19,7 +22,7 @@ const Contact: React.FC = () => {
 const ContactForm: React.FC = () => {
   return (
     <>
-      <h1 className="contactCTAHeader">Drop us a line!</h1>
+      <h1 className="contactHeader">Drop us a line!</h1>
       <input placeholder="Name" className="contactInput"></input>
       <input placeholder="Email Address" className="contactInput"></input>
       <input placeholder="Phone Number" className="contactInput"></input>
@@ -30,12 +33,20 @@ const ContactForm: React.FC = () => {
 };
 
 interface HeaderAndTextProps {
-  text: Array<string>;
+  header: string;
+  texts: Array<string>;
 }
 
-const HeaderAndText: React.FC<HeaderAndTextProps> = ({ text }) => {
+const HeaderAndText: React.FC<HeaderAndTextProps> = ({ header, texts }) => {
+
+  const textList = texts.map((text) =>
+    <p className="contactParagraph">{text}</p>
+  );
   return (
-    <></>
+    <div className="headerAndTextContainer">
+      <h1 className="contactHeader">{header}</h1>
+      {textList}
+    </div>
   );
 };
 
