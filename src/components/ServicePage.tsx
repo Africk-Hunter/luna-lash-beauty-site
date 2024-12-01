fc9736fd2406f528d186b30c4bfb58a6303b42fa
Africk-Hunter <hunterafrick@gmail.com>
2024-12-01 10:46:42 -0800

    Service Tab Switching
    - Added functionality to switch between different service tabs on desktop

--- src/components/ServicePage.tsx
+++ src/components/ServicePage.tsx
@@ -1,19 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CircleArrow from '../images/circle-arrow.svg';
 import PlusButton from '../images/plusButton.svg';
 import { lashServices, lashLifts, browServices, waxServices, facialServices } from './serviceInfo';
 
 interface ServicePageProps { }
 
+let serviceTabOptions = [
+    'Eyelash Extensions',
+    'Lash Lifts',
+    'Brow Services',
+    'Waxing',
+    'Facials',
+];
+let initialized = false;
+
 const ServicePage: React.FC<ServicePageProps> = () => {
     const [currentService, setCurrentService] = useState<number>(0);
     const [startX, setStartX] = useState<number>(0);
     const [startY, setStartY] = useState<number>(0);
 
-
     const handleTouchStart = (e: React.TouchEvent) => {
         setStartX(e.touches[0].clientX);
         setStartY(e.touches[0].clientY);
+    };
+
+    const handleTouchMove = (e: React.TouchEvent) => {
+        const currentX = e.touches[0].clientX;
+        const currentY = e.touches[0].clientY;
+
+        if (Math.abs(currentX - startX) > Math.abs(currentY - startY)) {
+            e.preventDefault();
+        }
     };
 
     const handleTouchEnd = (e: React.TouchEvent) => {
@@ -26,45 +43,68 @@ const ServicePage: React.FC<ServicePageProps> = () => {
         if (Math.abs(diffX) > Math.abs(diffY)) {
             if (diffX < -50 && currentService < 4) {
                 setCurrentService(currentService + 1);
-            }
-            else if (diffX > 50 && currentService > 0) {
+            } else if (diffX > 50 && currentService > 0) {
                 setCurrentService(currentService - 1);
             }
         }
     };
 
     return (
-        <section className="services" id="services" onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
-            <MobileLayout currentService={currentService} updateCurrentService={setCurrentService} />
+        <section
+            className="services"
+            id="services"
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+        >
+            <MainLayout currentService={currentService} updateCurrentService={setCurrentService} />
             <ServicesContainer currentService={currentService} />
         </section>
     );
 };
 
-interface MobileLayoutProps {
+interface MainLayoutProps {
     currentService: number;
     updateCurrentService: (newIndex: number) => void;
 }
 
-const MobileLayout: React.FC<MobileLayoutProps> = ({ currentService, updateCurrentService }) => {
-    const serviceTabOptions = [
-        'Eyelash Extensions',
-        'Lash Lifts',
-        'Brow Services',
-        'Waxing',
-        'Facials'
-    ];
+const MainLayout: React.FC<MainLayoutProps> = ({ currentService, updateCurrentService }) => {
+
+    const [windowWidth, setWindowWidth] = useState(0)
+
+    useEffect(() => {
+        const handleResize = () => setWindowWidth(window.innerWidth);
+
+        setWindowWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     const maxService = 4;
 
-    const updateServiceFunc = (plusOrMinus: 'plus' | 'minus') => {
+    const updateServiceFunc = (plusOrMinus: 'plus' | 'minus' | number) => {
         if (plusOrMinus === 'plus' && currentService < maxService) {
             updateCurrentService(currentService + 1);
         } else if (plusOrMinus === 'minus' && currentService > 0) {
             updateCurrentService(currentService - 1);
+        } else {
+            updateCurrentService(plusOrMinus);
         }
     };
-
+    if (windowWidth > 768) {
+        serviceTabOptions = [
+            'Brow Services',
+            'Lash Lifts',
+            'Eyelash Extensions',
+            'Waxing',
+            'Facials',
+        ]
+        if (!initialized){
+            updateServiceFunc(2)
+            initialized = true;
+        }
+    }
     return (
         <section className="servicesNavBar">
             <button
@@ -75,8 +115,26 @@ const MobileLayout: React.FC<MobileLayoutProps> = ({ currentService, updateCurre
                 <img src={CircleArrow} alt="Previous Service" className="circleButtonImg" />
             </button>
 
+
             <h2 className="currentServiceTab" id="currentServiceTab">
-                {serviceTabOptions[currentService]}
+                {windowWidth < 768 ? (
+                    <>
+                        {serviceTabOptions[currentService]}
+                    </>
+                ) : (
+                    <>
+
+                        {serviceTabOptions.map((serviceType, index) => (
+                            <button
+                                key={index}
+                                className={`currentServiceButton ${index === currentService ? 'active' : ''}`}
+                                onClick={() => updateServiceFunc(index)}
+                            >
+                                {serviceType}
+                            </button>
+                        ))}
+                    </>
+                )}
             </h2>
 
             <button
@@ -88,6 +146,7 @@ const MobileLayout: React.FC<MobileLayoutProps> = ({ currentService, updateCurre
             </button>
         </section>
     );
+
 };
 
 interface SubService {
@@ -129,14 +188,44 @@ const ServicesContainer: React.FC<ServicesContainerProps> = ({ currentService })
             break;
     }
 
-    const servicesList = services.map((service) => (
+    function expandServiceBox(index: number) {
+        const subServicesElement = document.querySelector(
+            `.subServices[data-index='${index}']`
+        ) as HTMLElement;
+
+        if (subServicesElement) {
+            subServicesElement.classList.toggle('visible');
+        }
+    }
+
+
+    const servicesList = services.map((service, index) => (
         <div className="serviceItem" key={service.title}>
             <div className="serviceInfo">
                 <div className="serviceInfoHolder top">
                     <p className="serviceItemInfo title">{service.title}</p>
                     <p className="serviceItemInfo">${service.price}</p>
                 </div>
+
+                {service.subServices && (
+                    <div className={`subServices ${index === 0 ? 'visible' : ''}`} data-index={index}>
+                        {service.subServices.map((subService, subIndex) => (
+                            <div key={subIndex} className="serviceInfoHolder">
+                                <p className="serviceItemInfo sub">{subService.title}</p>
+                                {subService.price && (
+                                    <p className="serviceItemInfo sub">${subService.price}</p>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
+
+            {service.subServices && (
+                <button className="serviceToggle" onClick={() => expandServiceBox(index)}>
+                    <img src={PlusButton} alt="Expand" />
+                </button>
+            )}
         </div>
     ));
 
