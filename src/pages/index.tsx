ac51d7dadd2f65ae085578dc092b625c8eb970f9
Africk-Hunter <hunterafrick@gmail.com>
2024-12-12 09:07:47 -0800

    Index Fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
+import '../styles/index.scss';
 
 const LandingPage: React.FC = () => {
   return (
