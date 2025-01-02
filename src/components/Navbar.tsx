f91a30256222df48b8b2e274df1a56b7771b2da1
Africk-Hunter <hunterafrick@gmail.com>
2025-01-01 18:33:17 -0800

    Navbar changes
    - Changed how navbar is render, since it was using a window size conditional rendering previously it was causing a weird look when loading a page. I changed it to adapt using CSS media breakpoints only which solves the problem.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import hamburger from '../images/hamburger.svg'
-import x from '../images/close.svg'
+import hamburger from '../images/hamburger.svg';
+import x from '../images/close.svg';
 
 const Navbar: React.FC = () => {
-
-  const [windowWidth, setWindowWidth] = useState(0)
-  const [navBarOpen, setNavBarOpen] = useState(false)
+  const [navBarOpen, setNavBarOpen] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
-      setWindowWidth(window.innerWidth);
+      setNavBarOpen(false);
     };
-    setWindowWidth(window.innerWidth);
     window.addEventListener('resize', handleResize);
     window.addEventListener('popstate', handleResize);
     return () => {
@@ -21,9 +18,7 @@ const Navbar: React.FC = () => {
   }, []);
 
   function toggleHamburger() {
-
     const navLinks = document.getElementById('navBarLinks');
-
     if (navLinks?.classList.contains('open')) {
       navLinks?.classList.remove('open');
       setNavBarOpen(false);
@@ -35,75 +30,43 @@ const Navbar: React.FC = () => {
 
   return (
     <section className='navbar' id='navbar'>
-      {windowWidth < 768 ? (
-        <>
-          <section className='topBarWrapper'>
-            <a href='/'><h1 className='companyName'>Luna Lash & Beauty Co.</h1></a>
-            <button className='hamburgerButton' onClick={toggleHamburger}><img src={ !navBarOpen ? hamburger : x} alt="Dropdown Menu" className='hamburgerImg' /></button>
-          </section>
-          <NavBarLinksWrapper windowWidth={windowWidth} names={['Home', 'Services', 'Gallery', 'About', 'FAQ']} />
-        </>
-      ) : (
-        <>
-          <section className='topBarWrapper'>
-            <a href='/'><h1 className='companyName'>Luna Lash & Beauty Co.</h1></a>
-            <NavBarLinksWrapper windowWidth={windowWidth} names={['Home', 'Services', 'Gallery', 'About', 'FAQ']} />
-          </section>
-        </>
-      )}
+      <section className='topBarWrapper'>
+        <a href='/'><h1 className='companyName'>Luna Lash & Beauty Co.</h1></a>
+        <button className='hamburgerButton' onClick={toggleHamburger}>
+          <img src={!navBarOpen ? hamburger : x} alt="Dropdown Menu" className='hamburgerImg' />
+        </button>
+      </section>
+      <NavBarLinksWrapper names={['Home', 'Services', 'Gallery', 'About', 'FAQ']} />
     </section>
   );
 };
 
 interface NavBarLinksProps {
-  windowWidth: number;
   names: Array<string>;
 }
-const NavBarLinksWrapper: React.FC<NavBarLinksProps> = ({ windowWidth, names }) => {
 
+const NavBarLinksWrapper: React.FC<NavBarLinksProps> = ({ names }) => {
   const linkItems = names.map((itemName, index) =>
-    <NavLinkButton windowWidth={windowWidth} name={itemName} key={index} />
-  )
+    <NavLinkButton name={itemName} key={index} />
+  );
 
   return (
-    <>
-      {windowWidth < 768 ? (
-        <nav className='navBarLinks mobile' id='navBarLinks'>
-          {linkItems}
-        </nav>
-      ) : (
-        <nav className='navBarLinks desktop'>
-          {linkItems}
-        </nav>
-      )}
-    </>
+    <nav className='navBarLinks' id='navBarLinks'>
+      {linkItems}
+    </nav>
   );
-}
-
+};
 
 interface NavLinkButtonProps {
-  windowWidth: number;
   name: string;
 }
-const NavLinkButton: React.FC<NavLinkButtonProps> = ({ windowWidth, name }) => {
 
-  let linkDirect: string = '';
-  if (name != 'Home') {
-    linkDirect = '/' + name.toLowerCase();
-  } else {
-    linkDirect = '/';
-  }
+const NavLinkButton: React.FC<NavLinkButtonProps> = ({ name }) => {
+  const linkDirect = name !== 'Home' ? `/${name.toLowerCase()}` : '/';
 
   return (
-    <>
-      {windowWidth < 768 ? (
-        <a href={linkDirect} className='navLinkButton mobile'>{name}</a>
-      ) : (
-        <a href={linkDirect} className='navLinkButton desktop'>{name}</a>
-      )}
-    </>
+    <a href={linkDirect} className='navLinkButton'>{name}</a>
   );
-
-}
+};
 
 export default Navbar;
